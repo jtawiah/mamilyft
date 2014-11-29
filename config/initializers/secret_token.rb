45fcb67664f8333713c7cffacbee5e8c1093c514
jtawiah <boatjohn87@gmail.com>
2014-11-29 15:30:07 +0000

    Start mamilyft_rydeshare_application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mamilyft::Application.config.secret_key_base = '7b249c9b7a65037c15e96870a9380a1390ecef0f67b1a73cd081b42dbde53bd7eb16ca630042dcfa37aa54765266497d75c92f0c246026613d1f11fa30c15e17'
